918fe98a2ad3ec0ba80fc3f06405d0c2573e39d1
TheAkseon <abqwerty390@gmail.com>
2023-03-17 15:45:23 +0300

    add colors on off for checkpoints

--- Assets/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour 
 {
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private Material _checkpointOn;
+    [SerializeField] private Material _checkpointOff;
+
     private HealthManager _healthManager;
 
     private void Start()
@@ -11,11 +15,29 @@ public class Checkpoint : MonoBehaviour
         _healthManager = FindObjectOfType<HealthManager>();
     }
 
+    public void CheckpointOn()
+    {
+        Checkpoint[] checkpoints = FindObjectsOfType<Checkpoint>();
+
+        foreach (Checkpoint checkpoint in checkpoints)
+        {
+            checkpoint.CheckpointOff();
+        }
+
+        _renderer.material = _checkpointOn;
+    }
+
+    public void CheckpointOff()
+    {
+        _renderer.material = _checkpointOff;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag.Equals("Player"))
         {
             _healthManager.SetSpawnPoint(transform.position);
+            CheckpointOn();
         }
     }
 }
