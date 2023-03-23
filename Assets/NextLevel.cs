91ee6d86fcc3e6dc6f79364b988577c814d376cf
TheAkseon <abqwerty390@gmail.com>
2023-03-23 16:42:30 +0300

    add three levels

---
+++ Assets/NextLevel.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class NextLevel : MonoBehaviour 
+{
+    private MenuManager _menuManager;
+
+    private void Start()
+    {
+        _menuManager = FindObjectOfType<MenuManager>();
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        _menuManager.NextLevel();
+    }
+}
---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour 
+{
+    [SerializeField] private string _levelName;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("FirstLevel");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(_levelName);
+    }
+}
