b78c2e260945486b52597a6c0022e91170abecb4
TheAkseon <abqwerty390@gmail.com>
2023-03-23 11:03:10 +0300

    add rotate platform

--- Assets/Scripts/Platforms/RotatePlatform.cs
+++ Assets/Scripts/Platforms/RotatePlatform.cs
@@ -1,8 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RotatePlatform : MonoBehaviour 
 {
+    [SerializeField] private float _speed;
 
+    private void Update()
+    {
+        transform.Rotate(0, _speed * Time.deltaTime, 0);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (((other.gameObject.tag == "Player") && other.GetComponent<CharacterController>().isGrounded))
+        {
+            other.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
 }
