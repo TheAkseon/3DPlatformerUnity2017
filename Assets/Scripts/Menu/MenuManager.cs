d78ceef0c9e1e02a60b32d00e4029bbabbcd9c38
TheAkseon <abqwerty390@gmail.com>
2023-03-27 11:59:52 +0300

    fix cursor bugs

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -5,6 +5,19 @@ public class MenuManager : MonoBehaviour
 {
     [SerializeField] private string _levelName;
 
+    private void Awake()
+    {
+        if(SceneManager.GetActiveScene().name == "Menu")
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("FirstLevel");
